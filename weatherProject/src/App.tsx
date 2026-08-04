185bb3aa37890d888740534db2d8d184ba9c543b
김승현 <kim0221_@naver.com>
2026-08-04 14:22:49 +0900

    modify: App.tsx style, Bottominfo 적용

--- weatherProject/src/App.tsx
+++ weatherProject/src/App.tsx
@@ -1,14 +1,19 @@
-import './App.css'
-import WeatherData from '../src/components/organism/weatherData/weatherData'
-import TimeTable from '../src/components/organism/timeTable/timeTable'
-function App() {
+import Header from './components/organism/header/header';
+import WeatherData from './components/organism/weatherData/weatherData';
+import TimeTable from './components/organism/timeTable/timeTable';
+import BottomInfo from './components/organism/bottomInfo/bottomInfo';
 
+function App() {
   return (
-    <>
-      <WeatherData />
-      <TimeTable />
-    </>
-  )
+    <div style={{ width: '100%', minHeight: '100vh', backgroundColor: '#0b0e1b' }}>
+      <Header />
+      <main>
+        <WeatherData />
+        <TimeTable />
+        <BottomInfo />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
